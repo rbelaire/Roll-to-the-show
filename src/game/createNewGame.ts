4743c0cb39bdc18537237ce4b8033d54ad4584b8
rbelaire <rjbelaire@gmail.com>
2026-02-04 22:31:17 -0600

    Update createNewGame.ts

--- src/game/createNewGame.ts
+++ src/game/createNewGame.ts
@@ -1,2 +1,19 @@
-import { GameState } from './GameState'  export function createNewGame(): GameState {   return {     inning: {       inningNumber: 1,       half: 'top'     },     outs: 0,     bases: {       first: false,       second: false,       third: false     },     home: { runs: 0 },     away: { runs: 0 },     gameOver: false   } }
+import { GameState } from './GameState'
 
+export function createNewGame(): GameState {
+  return {
+    inning: {
+      inningNumber: 1,
+      half: 'top'
+    },
+    outs: 0,
+    bases: {
+      first: false,
+      second: false,
+      third: false
+    },
+    home: { runs: 0 },
+    away: { runs: 0 },
+    gameOver: false
+  }
+}
